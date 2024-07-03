2baf9f9592ad2862a469fa5aba4241151d3034e3
Влад <grandisko@bk.ru>
2024-07-03 20:10:30 +0300

    инит

--- DB/__init__.py
+++ DB/__init__.py
@@ -1,6 +1,7 @@
 import os
 import sys
 
-
 # added directory with db
-sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),"forcrut")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),"Dima")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),"DB")))
