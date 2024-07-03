14a43a1423837fb4a90561f73d9221282a3433be
Влад <grandisko@bk.ru>
2024-07-03 20:12:27 +0300

    инит

--- forcrut/__init__.py
+++ forcrut/__init__.py
@@ -5,7 +5,9 @@ from .Authorization import AuthorizationWindow
 
 
 # added directory with app
-sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),"forcrut")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),"Dima")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),"DB")))
 
 
 app = QApplication(sys.argv)
